fdd5a86e8f944b9c7cb732ac67fc7cfe6d5e7cdf
SimonIsCoding <simon.archambault@hotmail.fr>
2026-02-20 14:33:49 +0800

    improving a bit

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,14 +1,15 @@
-char *s = "// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func();%c}";
+// char *s = "// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%cchar *s = %c%s%c;%c%s%c, 34, s, 34, 10, s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(s);%c}";
 // This is a comment
 #include <stdio.h>
 
-void print_func(char *s)
+// void print_func(char *s)
+void print_func(void)
 {
-  	printf("char *s = %c%s%c;%c%s", 34, s, 34, 10, s);
+  	printf("// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%c%c, 34, s, 34, 10, s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(s);%c}", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main(void)
 {
 	// Comment inside of main
-	print_func(s);
+	print_func();
 }

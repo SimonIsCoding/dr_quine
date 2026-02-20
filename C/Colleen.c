d111b388b79b84dd3b3d13653746312c7b00caf4
SimonIsCoding <simon.archambault@hotmail.fr>
2026-02-20 13:38:26 +0800

    starting on having something with the global variable

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,17 +1,14 @@
+char *s = "// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func();%c}";
 // This is a comment
 #include <stdio.h>
 
 void print_func(char *s)
 {
-	printf(s);
-	return ;
+  	printf("char *s = %c%s%c;%c%s", 34, s, 34, 10, s);
 }
 
 int main(void)
 {
-	// Comment inside of main 
-	print_func("calling print_func");
-	char *s = "Ma chaine est : %s";
-	printf(s, s);
-	return (0);
+	// Comment inside of main
+	print_func(s);
 }

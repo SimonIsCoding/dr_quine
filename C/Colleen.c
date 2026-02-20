72c87065d82364dcde8c5540200274790b6135db
SimonIsCoding <simon.archambault@hotmail.fr>
2026-02-20 15:30:53 +0800

    Collen Done :))))

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,15 +1,13 @@
-// char *s = "// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%cchar *s = %c%s%c;%c%s%c, 34, s, 34, 10, s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(s);%c}";
 // This is a comment
 #include <stdio.h>
 
-// void print_func(char *s)
-void print_func(void)
+void print_func(char *s)
 {
-  	printf("// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%c%c, 34, s, 34, 10, s);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(s);%c}", 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+  	printf("// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%c%s%c, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(%c%s%c);%c}", 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10);
 }
 
 int main(void)
 {
 	// Comment inside of main
-	print_func();
+	print_func("// This is a comment%c#include <stdio.h>%c%cvoid print_func(char *s)%c{%c  	printf(%c%s%c, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 34, s, 34, 10);%c}%c%cint main(void)%c{%c	// Comment inside of main%c	print_func(%c%s%c);%c}");
 }

3a84c61658f229b54743560ceb7800404721ac96
SimonIsCoding <simon.archambault@hotmail.fr>
2026-02-21 15:37:38 +0800

    working on Grace

--- C/Grace/Grace_kid.c
+++ C/Grace/Grace_kid.c
@@ -1,1 +1,11 @@
-"// this is a comment%cextern int open(const char *path, int oflag, ...);%cextern int write(int fildes, const void *buf, int nbyte);%c#define RUN int main(void) { dprintf(open(%cGrace_kid.c%c, 512 | 1, 0644), %c%s%c, 34, %s, 34 ); };%cRUN"
+// this is a comment
+extern int open(const char *path, int oflag, ...);
+extern int write(int fildes, const void *buf, int nbyte);
+extern int dprintf(int fd, const char *format, ...);
+#define SOURCE_CODE "// this is a comment
+extern int open(const char *path, int oflag, ...);
+extern int write(int fildes, const void *buf, int nbyte);
+#define RUN int main(void) { dprintf(open("Grace_kid.c", 512 | 1, 0644), "// this is a comment%cextern int open(const char *path, int oflag, ...);%cextern int write(int fildes, const void *buf, int nbyte);%c#define RUN int main(void) { dprintf(open(%cGrace_kid.c%c, 512 | 1, 0644), %c%s%c, 34, %s, 34 ); };%cRUN", 34, // this is a comment%cextern int open(const char *path, int oflag, ...);%cextern int write(int fildes, const void *buf, int nbyte);%c#define RUN int main(void) { dprintf(open(%cGrace_kid.c%c, 512 | 1, 0644), %c%s%c, 34, %s, 34 ); };%cRUN, 34 ); };
+RUN"
+#define RUN int main(void) { dprintf(open("Grace_kid.c", 512 | 1, 0644), ); };
+RUNN

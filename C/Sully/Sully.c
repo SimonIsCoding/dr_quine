ac56d60b8509f9bab121e5094b27649327c07d03
SimonIsCoding <simon.archambault@hotmail.fr>
2026-02-22 12:51:47 +0800

    files created

---
+++ C/Sully/Sully.c
@@ -0,0 +1,24 @@
+// il faut une boucle for/while
+// il faut une fonction qui crees des fichiers
+// il faut que la variable change au niveau de la creation du fichier
+// il faut gerer l'etat du compteur i
+
+// utiliser drpintf et sprintf
+// utiliser la fonction system
+#include <stdio.h>
+#include <fcntl.h>
+#define FILENAME
+
+int main(void)
+{
+	int	i = 5;
+	char filename[10];
+	while (i > 0)
+	{
+		snprintf(filename, sizeof(filename), "Sully_%d", i);
+		open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		printf("%s\n", filename);
+		i--;
+	}
+	return (0);
+}

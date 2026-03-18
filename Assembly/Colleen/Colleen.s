83d0c0515fc4dfa2414e5ec3cb6a1567203e4621
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-03-18 11:43:14 +0800

    I think it's done for COlleen

--- Assembly/Colleen/Colleen.s
+++ Assembly/Colleen/Colleen.s
@@ -11,6 +11,7 @@ Colleen:
 	mov		rsi, 34
 	mov		rdx, 10
 	lea		rcx, [quine]
+	xor		rax, rax
 	call	printf wrt ..plt
 	ret
 
@@ -21,4 +22,4 @@ main:
 	ret
 
 section	.data
-quine:		db "extern printf%2$cdefault rel%2$c%2$cglobal main%2$c%2$c;comment outside the entrypoint%2$c%2$csection .text%2$cColleen:%2$c	lea	rdi [quine]%2$c	lea	rsi [quine]%2$c	call	printf wrt ..plt%2$c	ret%2$c%2$cmain:%2$c;My comment inside the main%2$c	call	Colleen%2$c	xor	rax rax%2$c	ret%2$c%2$csection .data%2$cquine: db		%1$c%s%1$c, 0", 0
+quine:		db "extern printf%2$cdefault rel%2$c%2$cglobal main%2$c%2$c;comment outside the entrypoint%2$c%2$csection .text%2$cColleen:%2$c	lea	rdi, [quine]%2$c	mov	rsi, 34%2$c	mov	rdx, 10%2$c	lea	rcx, [quine]%2$c	call	printf wrt ..plt%2$c	ret%2$c%2$cmain:%2$c;My comment inside the main%2$c	call	Colleen%2$c	xor	rax rax%2$c	ret%2$c%2$csection .data%2$cquine: db		%1$c%3$s%1$c, 0", 0

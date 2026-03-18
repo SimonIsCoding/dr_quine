accfdbbd9b61c0dffc4dab11ae0bc4f25f1b88b2
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-03-18 17:37:51 +0800

    writing macros

--- Assembly/Grace/Grace.s
+++ Assembly/Grace/Grace.s
@@ -1,4 +1,23 @@
 ;syscall 2 for OPEN & 0x241 for flags
+%macro open 2
+	mov	rax, 2
+	lea	rdi, [filename]
+	mov	rsi, 0x241
+	syscall
+%endmacro
+
+%macro write 3
+	lea	rdi, [filename]
+	lea	rsi, code
+	mov	rdx, 7
+	syscall
+%endmacro
+
+%macro close
+	lea	rdi, [filename]
+	syscall
+%endmacro
+
 default	rel
 global	main
 
@@ -12,4 +31,7 @@ main:
 
 section	.data
 filename:	db	"Grace_kid.s", 0
+code:		db	"myCode", 0
 ;int open("file.s", O_WRONLY | O_CREAT | O_TRUNC)
+;int write(int fd, char *str, ssize_t buf)
+;int close

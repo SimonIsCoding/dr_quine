ca6350c6d2733a238634c76be2c4f3cf0295c697
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-03-19 09:19:43 +0800

    fileCreated now

--- Assembly/Grace/Grace.s
+++ Assembly/Grace/Grace.s
@@ -1,37 +1,42 @@
-;syscall 2 for OPEN & 0x241 for flags
-%macro open 2
-	mov	rax, 2
-	lea	rdi, [filename]
-	mov	rsi, 0x241
-	syscall
-%endmacro
+	;syscall 2 for OPEN & 0x241 for flags
+	%macro open 3
+		mov	rax, 2
+		lea	rdi, %1
+		mov	rsi, %2
+		mov	rdx, %3
+		syscall
+	%endmacro
 
-%macro write 3
-	lea	rdi, [filename]
-	lea	rsi, code
-	mov	rdx, 7
-	syscall
-%endmacro
+	%macro write 3
+		mov	rax, 1
+		mov	rdi, %1
+		lea	rsi, %2
+		mov	rdx, %3
+		syscall
+	%endmacro
 
-%macro close
-	lea	rdi, [filename]
-	syscall
-%endmacro
+	%macro close 1
+		mov	rax, 3
+		mov	rdi, %1
+		syscall
+	%endmacro
 
-default	rel
-global	main
+	default	rel	
+	global	main
 
-section	.text
-main:
-	mov	rax, 2
-	lea	rdi, [filename]
-	mov	rsi, 0x241
-	syscall
-	ret
+	section	.text
+	main:
+		open	[filename], 0x241, 644o
+		mov		[fd], rax
+		write	[fd], [code], 7
+		close	[fd]
+		xor		rax, rax
+		ret
 
-section	.data
-filename:	db	"Grace_kid.s", 0
-code:		db	"myCode", 0
-;int open("file.s", O_WRONLY | O_CREAT | O_TRUNC)
-;int write(int fd, char *str, ssize_t buf)
-;int close
+	section	.data
+	filename:	db	"Grace_kid.s", 0
+	code:		db	"myCode", 0
+	fd:			dq	0
+	;int open("file.s", O_WRONLY | O_CREAT | O_TRUNC)
+	;int write(int fd, char *str, ssize_t buf)
+	;int close

58c07fbd18de5a08d8b9e65bd1226c2018afc973
SimonIsCoding <sim.arch@proton.me>
2026-03-30 16:38:49 +0200

    Grace.s ready

--- Assembly/Grace/Grace.s
+++ Assembly/Grace/Grace.s
@@ -22,7 +22,7 @@ extern	dprintf
 	syscall
 %endmacro
 
-default	rel	
+default	rel
 global	main
 
 section	.text
@@ -46,5 +46,5 @@ main:
 
 section	.data
 filename:	db	"Grace_kid.s", 0
-code:		db	";syscall 2 for OPEN & 0x241 for flags%2$cextern	dprintf%2$c%%macro open 3%2$c%%endmacro%2$csection	.text%2$cmain:%2$c	push	rbp%2$c%2$c	open	[filename], 0x241, 644o%2$c	mov		[fd], rax%2$c	mov		rdi, [fd]%2$c	lea		rsi, [code]%2$c	lea		rdx, [code]%2$c	mov		rcx, 10%2$c	mov		r8, 34%2$c	xor		rax, rax%2$c	call	dprintf wrt ..plt%2$c	close	[fd]%2$c	xor		rax, rax%2$c%2$c	pop		rbp%2$c	ret%2$c%2$csection	.data%2$cfilename:	db	%3$cGrace_kid.s%3$c, 0%2$ccode:		db	%3$c%s%3$c, 0%2$cfd:			dq	0", 0
+code:		db	";syscall 2 for OPEN & 0x241 for flags%2$cextern	dprintf%2$c%%macro open 3%2$c	mov	rax, 2%2$c	lea	rdi, %%1%2$c	mov	rsi, %%2%2$c	mov	rdx, %%3%2$c	syscall%2$c%%endmacro%2$c%2$c%%macro write 3%2$c	mov	rax, 1%2$c	mov	rdi, %%1%2$c	lea	rsi, %%2%2$c	mov	rdx, %%3%2$c	syscall%2$c%%endmacro%2$c%2$c%%macro close 1%2$c	mov	rax, 3%2$c	mov	rdi, %%1%2$c	syscall%2$c%%endmacro%2$c%2$cdefault	rel%2$cglobal	main%2$c%2$csection	.text%2$cmain:%2$c	push	rbp%2$c%2$c	open	[filename], 0x241, 644o%2$c	mov		[fd], rax%2$c	mov		rdi, [fd]%2$c	lea		rsi, [code]%2$c	lea		rdx, [code]%2$c	mov		rcx, 10%2$c	mov		r8, 34%2$c	xor		rax, rax%2$c	call	dprintf wrt ..plt%2$c	close	[fd]%2$c	xor		rax, rax%2$c%2$c	pop		rbp%2$c	ret%2$c%2$csection	.data%2$cfilename:	db	%3$cGrace_kid.s%3$c, 0%2$ccode:		db	%3$c%s%3$c, 0%2$cfd:			dq	0", 0
 fd:			dq	0
--- Assembly/Grace/Grace_kid.s
+++ Assembly/Grace/Grace_kid.s
@@ -1,7 +1,30 @@
 ;syscall 2 for OPEN & 0x241 for flags
 extern	dprintf
 %macro open 3
+	mov	rax, 2
+	lea	rdi, %1
+	mov	rsi, %2
+	mov	rdx, %3
+	syscall
 %endmacro
+
+%macro write 3
+	mov	rax, 1
+	mov	rdi, %1
+	lea	rsi, %2
+	mov	rdx, %3
+	syscall
+%endmacro
+
+%macro close 1
+	mov	rax, 3
+	mov	rdi, %1
+	syscall
+%endmacro
+
+default	rel
+global	main
+
 section	.text
 main:
 	push	rbp
@@ -23,5 +46,5 @@ main:
 
 section	.data
 filename:	db	"Grace_kid.s", 0
-code:		db	";syscall 2 for OPEN & 0x241 for flags%2$cextern	dprintf%2$c%%macro open 3%2$c%%endmacro%2$csection	.text%2$cmain:%2$c	push	rbp%2$c%2$c	open	[filename], 0x241, 644o%2$c	mov		[fd], rax%2$c	mov		rdi, [fd]%2$c	lea		rsi, [code]%2$c	lea		rdx, [code]%2$c	mov		rcx, 10%2$c	mov		r8, 34%2$c	xor		rax, rax%2$c	call	dprintf wrt ..plt%2$c	close	[fd]%2$c	xor		rax, rax%2$c%2$c	pop		rbp%2$c	ret%2$c%2$csection	.data%2$cfilename:	db	%3$cGrace_kid.s%3$c, 0%2$ccode:		db	%3$c%s%3$c, 0%2$cfd:			dq	0", 0
+code:		db	";syscall 2 for OPEN & 0x241 for flags%2$cextern	dprintf%2$c%%macro open 3%2$c	mov	rax, 2%2$c	lea	rdi, %%1%2$c	mov	rsi, %%2%2$c	mov	rdx, %%3%2$c	syscall%2$c%%endmacro%2$c%2$c%%macro write 3%2$c	mov	rax, 1%2$c	mov	rdi, %%1%2$c	lea	rsi, %%2%2$c	mov	rdx, %%3%2$c	syscall%2$c%%endmacro%2$c%2$c%%macro close 1%2$c	mov	rax, 3%2$c	mov	rdi, %%1%2$c	syscall%2$c%%endmacro%2$c%2$cdefault	rel%2$cglobal	main%2$c%2$csection	.text%2$cmain:%2$c	push	rbp%2$c%2$c	open	[filename], 0x241, 644o%2$c	mov		[fd], rax%2$c	mov		rdi, [fd]%2$c	lea		rsi, [code]%2$c	lea		rdx, [code]%2$c	mov		rcx, 10%2$c	mov		r8, 34%2$c	xor		rax, rax%2$c	call	dprintf wrt ..plt%2$c	close	[fd]%2$c	xor		rax, rax%2$c%2$c	pop		rbp%2$c	ret%2$c%2$csection	.data%2$cfilename:	db	%3$cGrace_kid.s%3$c, 0%2$ccode:		db	%3$c%s%3$c, 0%2$cfd:			dq	0", 0
 fd:			dq	0

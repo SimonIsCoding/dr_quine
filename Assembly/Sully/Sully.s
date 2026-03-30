67166ad1bac4667b99493b3dc661f94cb78e3003
SimonIsCoding <sim.arch@proton.me>
2026-03-30 17:30:13 +0200

    changing Sully.c filename -> buffer

--- Assembly/Sully/Sully.s
+++ Assembly/Sully/Sully.s
@@ -1,42 +1,44 @@
-	;syscall 2 for OPEN & 0x241 for flags
-	%macro open 3
-		mov	rax, 2
-		lea	rdi, %1
-		mov	rsi, %2
-		mov	rdx, %3
-		syscall
-	%endmacro
+%macro open 3
+	mov	rax, 2
+	lea	rdi, %1
+	mov	rsi, %2
+	mov	rdx, %3
+	syscall
+%endmacro
 
-	%macro write 3
-		mov	rax, 1
-		mov	rdi, %1
-		lea	rsi, %2
-		mov	rdx, %3
-		syscall
-	%endmacro
+%macro close 1
+	mov	rax, 3
+	mov	rdi, %1
+	syscall
+%endmacro
 
-	%macro close 1
-		mov	rax, 3
-		mov	rdi, %1
-		syscall
-	%endmacro
+extern	snprintf
 
-	default	rel	
-	global	main
+default	rel	
+global	main
 
-	section	.text
-	main:
-		open	[filename], 0x241, 644o
-		mov		[fd], rax
-		write	[fd], [code], 7
-		close	[fd]
-		xor		rax, rax
-		ret
+section	.text
+main:
+	push	rbp
+;	open	[filename], 0x241, 644o
+	lea		rdi, [buf]
+	mov		rsi, 32
+	mov		rcx, counter
+	lea		rdx, [filename]
+	call	snprintf wrt ..plt
+	mov		[fd], rax
+	write	[fd], [code], 7
+	close	[fd]
+	xor		rax, rax
+	pop		rbp
+	ret
 
-	section	.data
-	filename:	db	"Grace_kid.s", 0
-	code:		db	"myCode", 0
-	fd:			dq	0
-	;int open("file.s", O_WRONLY | O_CREAT | O_TRUNC)
-	;int write(int fd, char *str, ssize_t buf)
-	;int close
+section	.data
+filename:	db	"Sully_%d.s", 0
+code:		db	"myCode", 0
+fd:			dq	0
+counter		dq	5
+
+section	.bss
+buf:		resb	32
+file_size:	resb	32

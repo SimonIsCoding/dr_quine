df04fa2efd29d2841a7e1b79eacc76389610b66a
SimonIsCoding <sim.arch@proton.me>
2026-03-30 16:25:13 +0200

    almost done with Grace

--- Assembly/Grace/Grace.s
+++ Assembly/Grace/Grace.s
@@ -1,43 +1,56 @@
+;syscall 2 for OPEN & 0x241 for flags
+extern	dprintf
+%macro open 3
+	mov	rax, 2
+	lea	rdi, %1
+	mov	rsi, %2
+	mov	rdx, %3
+	syscall
+%endmacro
+
+%macro write 3
+	mov	rax, 1
+	mov	rdi, %1
+	lea	rsi, %2
+	mov	rdx, %3
+	syscall
+%endmacro
+
+%macro close 1
+	mov	rax, 3
+	mov	rdi, %1
+	syscall
+%endmacro
+
 ;en gros tu dois utiliser dprintf, open, write. close ?
-	;syscall 2 for OPEN & 0x241 for flags
-	%macro open 3
-		mov	rax, 2
-		lea	rdi, %1
-		mov	rsi, %2
-		mov	rdx, %3
-		syscall
-	%endmacro
 
-	%macro write 3
-		mov	rax, 1
-		mov	rdi, %1
-		lea	rsi, %2
-		mov	rdx, %3
-		syscall
-	%endmacro
+default	rel	
+global	main
 
-	%macro close 1
-		mov	rax, 3
-		mov	rdi, %1
-		syscall
-	%endmacro
+section	.text
+main:
+	push	rbp
 
-	default	rel	
-	global	main
+	open	[filename], 0x241, 644o
+	mov		[fd], rax
+	mov		rdi, [fd]
+	lea		rsi, [code]
+	lea		rdx, [code]
+	mov		rcx, 10
+	mov		r8, 34
+	xor		rax, rax
+	call	dprintf wrt ..plt
+	close	[fd]
+	xor		rax, rax
 
-	section	.text
-	main:
-		open	[filename], 0x241, 644o
-		mov		[fd], rax
-		write	[fd], [code], 7
-		close	[fd]
-		xor		rax, rax
-		ret
+	pop		rbp
+	ret
 
-	section	.data
-	filename:	db	"Grace_kid.s", 0
-	code:		db	"myCode", 0
-	fd:			dq	0
-	;int open("file.s", O_WRONLY | O_CREAT | O_TRUNC)
-	;int write(int fd, char *str, ssize_t buf)
-	;int close
+section	.data
+filename:	db	"Grace_kid.s", 0
+code:		db	";syscall 2 for OPEN & 0x241 for flags%2$cextern	dprintf%2$csection	.text%2$cmain:%2$c	push	rbp%2$c%2$c	open	[filename], 0x241, 644o%2$c	mov		[fd], rax%2$c	mov		rdi, [fd]%2$c	lea		rsi, [code]%2$c	lea		rdx, [code]%2$c	mov		rcx, 10%2$c	xor		rax, rax%2$c	call	dprintf wrt ..plt%2$c	close	[fd]%2$c	xor		rax, rax%2$c%2$c	pop		rbp%2$c	ret%2$c%2$csection	.data%2$cfilename:	db	%3$cGrace_kid.s%3$c, 0%2$ccode:		db	%3$c%s%3$c, 0", 0
+fd:			dq	0
+;int open(file.s, O_WRONLY | O_CREAT | O_TRUNC)
+;int write(int fd, char *str, ssize_t buf)
+;int close
+;int dprintf(int fd, const char *format, ...);

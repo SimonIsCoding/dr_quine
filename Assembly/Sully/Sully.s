ac1d44f3c6f86d990288944acb1e9384e94b555d
SimonIsCoding <sim.arch@proton.me>
2026-05-05 09:05:36 +0200

    going forward little by little

--- Assembly/Sully/Sully.s
+++ Assembly/Sully/Sully.s
@@ -45,7 +45,7 @@ main:
 	mov		rax, [counter]
 	dec		rax
 	mov		[counter], rax
-	
+
 	open	[fileName], 0x241, 644o
 
 	mov		[fd], rax
@@ -54,7 +54,7 @@ main:
 	lea		rdx, 10
 	mov		rcx, 34
 	mov		r8, [counter]
-	mov		r9, [code]
+	lea		r9, [code]
 	xor		rax, rax
 	call	dprintf wrt ..plt
 
@@ -70,4 +70,4 @@ fd:			resb	32
 section	.data
 counter:	dq	5
 name:		db	"Sully_%d.s", 0
-code:		db	"extern	dprintf%1$cextern	snprintf%1$c%1$c%%macro open 3%1$c	mov	rax, 2%1$c	lea	rdi, %%1%1$c	mov	rsi, %%2%1$c	mov	rdx, %%3%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro close 1%1$c	mov	rax, 3%1$c	mov	rdi, %%1%1$c	syscall%1$c%%endmacro%1$c%1$cglobal main%1$c%1$csection	.text%1$cmain:%1$c	push	rbp%1$c	xor		rax, rax%1$c%1$c	lea 	rdi, [fileName]%1$c	mov		rsi, 32%1$c	lea		rdx, [name]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rax, [counter]%1$c	dec		rax%1$c	mov		[counter], rax%1$c%1$c	open	[fileName], 0x241, 644o%1$c%1$c	mov		[fd], rax%1$c	mov		rdi, [fd]%1$c	lea		rsi, [code]%1$c	lea		rdx, 10%1$c	mov		rcx, 34%1$c	mov		r8, [%3$c]%1$c	mov		r9, [code]%1$c	xor		rax, rax%1$c	call	dprintf wrt ..plt%1$c%1$c	close	[fd]%1$c%1$c	pop		rbp%1$c	ret%1$c%1$csection .bss%1$cfileName:	resb	32%1$cfd:			resb	32%1$c%1$csection	.data%1$ccounter:	dq	%3$c%1$cname:		db	%2$cSully_%%d.s%2$c, 0%1$c", 0
+code:		db	"extern	dprintf%1$cextern	snprintf%1$c%1$c%%macro open 3%1$c	mov	rax, 2%1$c	lea	rdi, %%1%1$c	mov	rsi, %%2%1$c	mov	rdx, %%3%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro close 1%1$c	mov	rax, 3%1$c	mov	rdi, %%1%1$c	syscall%1$c%%endmacro%1$c%1$cglobal main%1$c%1$csection	.text%1$cmain:%1$c	push	rbp%1$c	xor		rax, rax%1$c%1$c	lea 	rdi, [fileName]%1$c	mov		rsi, 32%1$c	lea		rdx, [name]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rax, [counter]%1$c	dec		rax%1$c	mov		[counter], rax%1$c%1$c	open	[fileName], 0x241, 644o%1$c%1$c	mov		[fd], rax%1$c	mov		rdi, [fd]%1$c	lea		rsi, [code]%1$c	lea		rdx, 10%1$c	mov		rcx, 34%1$c	mov		r8, [counter]%1$c	lea		r9, [code]%1$c	xor		rax, rax%1$c	call	dprintf wrt ..plt%1$c%1$c	close	[fd]%1$c%1$c	pop		rbp%1$c	ret%1$c%1$csection .bss%1$cfileName:	resb	32%1$cfd:			resb	32%1$c%1$csection	.data%1$ccounter:	dq	%3$d%1$cname:		db	%2$cSully_%%d.s%2$c, 0%1$ccode:		db	%2$c%4$s%2$c, 0", 0
--- Assembly/Sully/Sully_5.s
+++ Assembly/Sully/Sully_5.s
@@ -39,7 +39,7 @@ main:
 	lea		rsi, [code]
 	lea		rdx, 10
 	mov		rcx, 34
-	mov		r8, []
+	mov		r8, [counter]
 	mov		r9, [code]
 	xor		rax, rax
 	call	dprintf wrt ..plt
@@ -54,5 +54,6 @@ fileName:	resb	32
 fd:			resb	32
 
 section	.data
-counter:	dq	
+counter:	dq	4
 name:		db	"Sully_%d.s", 0
+code:		db	"extern	dprintf%1$cextern	snprintf%1$c%1$c%%macro open 3%1$c	mov	rax, 2%1$c	lea	rdi, %%1%1$c	mov	rsi, %%2%1$c	mov	rdx, %%3%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro close 1%1$c	mov	rax, 3%1$c	mov	rdi, %%1%1$c	syscall%1$c%%endmacro%1$c%1$cglobal main%1$c%1$csection	.text%1$cmain:%1$c	push	rbp%1$c	xor		rax, rax%1$c%1$c	lea 	rdi, [fileName]%1$c	mov		rsi, 32%1$c	lea		rdx, [name]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rax, [counter]%1$c	dec		rax%1$c	mov		[counter], rax%1$c%1$c	open	[fileName], 0x241, 644o%1$c%1$c	mov		[fd], rax%1$c	mov		rdi, [fd]%1$c	lea		rsi, [code]%1$c	lea		rdx, 10%1$c	mov		rcx, 34%1$c	mov		r8, [counter]%1$c	mov		r9, [code]%1$c	xor		rax, rax%1$c	call	dprintf wrt ..plt%1$c%1$c	close	[fd]%1$c%1$c	pop		rbp%1$c	ret%1$c%1$csection .bss%1$cfileName:	resb	32%1$cfd:			resb	32%1$c%1$csection	.data%1$ccounter:	dq	%3$d%1$cname:		db	%2$cSully_%%d.s%2$c, 0%1$ccode:		db	%2$c%4$s%2$c, 0", 0

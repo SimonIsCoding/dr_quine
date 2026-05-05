164e7df3955dfa87c23cb2f1b3cb69170c065be5
SimonIsCoding <sim.arch@proton.me>
2026-05-05 10:36:13 +0200

    nasm command created inside of the Sully.s file

--- Assembly/Sully/Sully.s
+++ Assembly/Sully/Sully.s
@@ -14,6 +14,7 @@
 
 extern	dprintf
 extern	snprintf
+extern	system
 
 %macro open 3
 	mov	rax, 2
@@ -47,6 +48,12 @@ main:
 	mov		rcx, [counter]
 	call	snprintf wrt ..plt
 
+	lea 	rdi, [objectName]
+	mov		rsi, 32
+	lea		rdx, [object]
+	mov		rcx, [counter]
+	call	snprintf wrt ..plt
+
 	mov		rax, [counter]
 	dec		rax
 	mov		[counter], rax
@@ -63,6 +70,17 @@ main:
 	xor		rax, rax
 	call	dprintf wrt ..plt
 
+;int snprintf(execution1, 64, "nasm -f elf64 %s -o %s", fileName, objectName);
+	mov		rdi, execution1
+	mov		rsi, 64
+	lea		rdx, [nasm]
+	lea		rcx, [fileName]
+	lea		r8, [objectName]
+	call	snprintf wrt ..plt
+
+	mov		rdi, execution1
+	call	system wrt ..plt
+
 	close	[fd]
 
 .end:
@@ -71,9 +89,14 @@ main:
 
 section .bss
 fileName:	resb	32
+objectName:	resb	32
 fd:			resb	32
+execution1:	resb	64
 
 section	.data
 counter:	dq	5
 name:		db	"Sully_%d.s", 0
-code:		db	"extern	dprintf%1$cextern	snprintf%1$c%1$c%%macro open 3%1$c	mov	rax, 2%1$c	lea	rdi, %%1%1$c	mov	rsi, %%2%1$c	mov	rdx, %%3%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro close 1%1$c	mov	rax, 3%1$c	mov	rdi, %%1%1$c	syscall%1$c%%endmacro%1$c%1$cglobal main%1$c%1$csection	.text%1$cmain:%1$c	push	rbp%1$c	xor		rax, rax%1$c%1$c	mov		rax, [counter]%1$c	cmp		rax, -1%1$c	je		.end%1$c	xor		rax, rax%1$c%1$c	lea 	rdi, [fileName]%1$c	mov		rsi, 32%1$c	lea		rdx, [name]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rax, [counter]%1$c	dec		rax%1$c	mov		[counter], rax%1$c%1$c	open	[fileName], 0x241, 644o%1$c%1$c	mov		[fd], rax%1$c	mov		rdi, [fd]%1$c	lea		rsi, [code]%1$c	lea		rdx, 10%1$c	mov		rcx, 34%1$c	mov		r8, [counter]%1$c	lea		r9, [code]%1$c	xor		rax, rax%1$c	call	dprintf wrt ..plt%1$c%1$c	close	[fd]%1$c%1$c.end:%1$c	pop		rbp%1$c	ret%1$c%1$csection .bss%1$cfileName:	resb	32%1$cfd:			resb	32%1$c%1$csection	.data%1$ccounter:	dq	%3$d%1$cname:		db	%2$cSully_%%d.s%2$c, 0%1$ccode:		db	%2$c%4$s%2$c, 0", 0
+object:		db	"Sully_%d.o", 0
+executable:	db	"Sully_%d", 0
+nasm:		db	"nasm -f elf64 %s -o %s", 0
+code:		db	"extern	dprintf%1$cextern	snprintf%1$cextern	system%1$c%1$c%%macro open 3%1$c	mov	rax, 2%1$c	lea	rdi, %%1%1$c	mov	rsi, %%2%1$c	mov	rdx, %%3%1$c	syscall%1$c%%endmacro%1$c%1$c%%macro close 1%1$c	mov	rax, 3%1$c	mov	rdi, %%1%1$c	syscall%1$c%%endmacro%1$c%1$cglobal main%1$c%1$csection	.text%1$cmain:%1$c	push	rbp%1$c	xor		rax, rax%1$c%1$c	mov		rax, [counter]%1$c	cmp		rax, -1%1$c	je		.end%1$c	xor		rax, rax%1$c%1$c	lea 	rdi, [fileName]%1$c	mov		rsi, 32%1$c	lea		rdx, [name]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	lea 	rdi, [objectName]%1$c	mov		rsi, 32%1$c	lea		rdx, [object]%1$c	mov		rcx, [counter]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rax, [counter]%1$c	dec		rax%1$c	mov		[counter], rax%1$c%1$c	open	[fileName], 0x241, 644o%1$c%1$c	mov		[fd], rax%1$c	mov		rdi, [fd]%1$c	lea		rsi, [code]%1$c	lea		rdx, 10%1$c	mov		rcx, 34%1$c	mov		r8, [counter]%1$c	lea		r9, [code]%1$c	xor		rax, rax%1$c	call	dprintf wrt ..plt%1$c%1$c	mov		rdi, execution1%1$c	mov		rsi, 64%1$c	lea		rdx, [nasm]%1$c	lea		rcx, [fileName]%1$c	lea		r8, [object]%1$c	call	snprintf wrt ..plt%1$c%1$c	mov		rdi, execution1%1$c	call	system wrt ..plt%1$c%1$c	close	[fd]%1$c%1$c.end:%1$c	pop		rbp%1$c	ret%1$c%1$csection .bss%1$cfileName:	resb	32%1$cobjectName:	resb	32%1$cfd:			resb	32%1$cexecution1:	resb	64%1$c%1$csection	.data%1$ccounter:	dq	%3$d%1$cname:		db	%2$cSully_%%d.s%2$c, 0%1$cobject:		db	%2$cSully_%%d.o%2$c, 0%1$cexecutable:		db	%2$cSully_%%d%2$c, 0%1$cnasm:		db	%2$cnasm -f elf64 %%s -o %%s%2$c, 0%1$ccode:		db	%2$c%4$s%2$c, 0", 0
